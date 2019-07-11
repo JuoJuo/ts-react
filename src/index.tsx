cc907466d28d8890aa0f253f77034c250425dc18
李人杰 <li.ren.jie@navercorp.com>
2019-07-11 14:55:37 +0800

    增加react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,34 @@ import TodoInput from "./components/TodoInput";
 import TodoItem from "./components/TodoItem";
 import Counter from "./components/Counter";
 import StoredCounter from "./components/StoredCounter";
-import {Provider} from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <Counter name="不带redux的counter"/>
-    <hr/>
-    <TodoItem title="函数组件的ts示例"/>
-    <hr/>
-    <StoredCounter name="带有redux的counter"/>
-    <hr/>
-    <TodoInput />
+    <Router>
+      <>
+        <Link to="/a">按钮1</Link>
+        <Link to="/b">按钮2</Link>
+        <Link to="/c">按钮3</Link>
+        <Link to="/d">按钮4</Link>
+        <hr/>
+        <Route
+          path="/a"
+          component={() => <Counter name="不带redux的counter" />}
+        />
+        <Route
+          path="/b"
+          component={() => <TodoItem title="函数组件的ts示例" />}
+        />
+        <Route
+          path="/c"
+          component={() => <StoredCounter name="带有redux的counter" />}
+        />
+        <Route path="/d" component={() => <TodoInput />} />
+      </>
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
